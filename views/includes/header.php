8ac8b69893a55e68174ca7140176bab1a325b59f
angestephane <boguhestephane97@gmail.com>
2020-08-30 00:16:05 +0100

    modification du menu principal

--- views/includes/header.php
+++ views/includes/header.php
@@ -1,33 +1,37 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark"> 
+<header>
+  <nav class="navbar navbar-expand-lg item-align-center mt-2 mb-0"> 
+      
+      
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
     
-    <a class="navbar-brand" href="#">StephBlog</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  
- 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item <?php if($_GET['page']==="home"): ?>active<?php endif ?>" >
-          <a class="nav-link" href="home">Accueil</a>
-        </li>
-        
-        <li class="nav-item <?php if($_GET['page']==="newspost"): ?>active<?php endif ?>">
-          <a class="nav-link" href="newspost">Nouveau Poste</a>
-        </li>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ">
+          <li class="nav-item <?php if($_GET['page']==="home"): ?>active<?php endif ?>" >
+            <a class="nav-link text-dark" href="home">Accueil</a>
+          </li>
+          
+          <li class="nav-item <?php if($_GET['page']==="newspost"): ?>active<?php endif ?>">
+            <a class="nav-link text-dark" href="newspost">Nouveau Poste</a>
+          </li>
+        </ul>
 
-      </ul>
-          <?php if(isset($_SESSION['name'])) : ?>
-          <h6 class="text-light name-login"><?=$_SESSION['name']?>
-            <a href="deconection"><img src="../assets/img/logout.png" alt="Déconnexion" title="Déconnexion" class="img-logout"></a>
-          </h6>
-          <?php endif ?>
-  </div>
-</nav>
-<style>
-      .active{
-          border-bottom:2px solid white;
-      }
-    </style>
+        <a class="navbar-brand mx-auto text-dark" href="#">OnEnParle</a>
+      </div>
+
+            <?php if(isset($_SESSION['name'])) : ?>
+            <h6 class="text-dark name-login"><?=$_SESSION['name']?>
+              <a href="deconection"><img src="../assets/img/logout.png" alt="Déconnexion" title="Déconnexion" class="img-logout"></a>
+            </h6>
+            <?php endif ?>
+  
+  </nav>
+  <style>
+        .active{
+            border-bottom:2px solid white;
+        }
+  </style>
 
 </header>
+<hr>
